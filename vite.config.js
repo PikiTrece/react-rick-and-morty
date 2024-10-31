b63fcbb3c445475fe8e08a7c6215f32b27b47882
PikiTrece <piki.dawdam@gmail.com>
2024-10-31 13:35:11 +0100

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: "https://PikiTrece.github.io/react-rick-and-morty/",
-  plugins: [react()],
-})
